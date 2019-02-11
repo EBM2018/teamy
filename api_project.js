b3aa3b59f786f8747494569cad6e10794fca4def
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-11 09:19:46 +0000

    Deploy EBM2018/teamy to github.com/EBM2018/teamy.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-30T16:22:36.579Z",
+    "time": "2019-02-11T09:19:16.963Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
