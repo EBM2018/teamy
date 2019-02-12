644c5a0ba0d6b1fbc1eb7324934d9c4b673a3f0c
Nathan <n.malnoury@gmail.com>
2019-02-12 08:33:30 +0100

    Firsts Routes implemented
    Changed Arhitecture of the filesystem to match best practises
    User Schema done:
    User Controller began;
    Config mostly done;
    Proflist Schema done;

---
+++ backend/src/api/resources/user/userController.js
@@ -0,0 +1,13 @@
+// const UserData = require('../../../services/user/model');
+
+const controller = {
+  index(req, res) {
+    return res.status(200).send('INDEX TO BE DEVELOPED');
+  },
+
+  eleve_list(req, res) {
+    return res.send('LIST NOT IMPLEMENTED YET');
+  },
+};
+
+module.exports = controller;
