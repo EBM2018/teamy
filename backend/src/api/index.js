501bc749fa6a69d65d1caa0556d0cb36f2874b69
Nathan <n.malnoury@gmail.com>
2019-02-26 11:20:22 +0100

    First implementation Prof-group api

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,5 +3,6 @@ const { Router } = require('express');
 const router = new Router();
 
 router.use('/user', require('./resources/user'));
+router.use('/prof-group', require('./resources/profGroup'));
 
 module.exports = router;
