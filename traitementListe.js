87d470bab4ead2be7370ad533657cbec8c002161
gvallet <gabin.vallet5@gmail.com>
2019-02-26 08:50:55 +0100

    Auto stash before rebase of "origin/GabinDev"

--- traitementListe.js
+++
@@ -1,8 +0,0 @@
-function selectClasse(){
-    var list = document.getElementById('list');
-    list.addEventListener('change',function(){
-        var nom=list.options[list.selectedIndex].innerHTML;
-        alert(nom);
-        document.cookie = "nomClasse=" + nom;
-    });
-}
