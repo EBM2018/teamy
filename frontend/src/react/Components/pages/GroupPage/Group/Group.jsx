4208acfb005e589c6ef256e3bcdca7010f5336c6
Cyril <cyril.brajon96@live.fr>
2019-03-04 10:37:34 +0100

    Add page gestion des groupes, sliders link to number of group

--- frontend/src/react/Components/pages/GroupPage/Group/Group.jsx
+++ frontend/src/react/Components/pages/GroupPage/Group/Group.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+//import classNames from "../GroupsArea/groupsArea.module.css";
 //import PropTypes from 'prop-types';
 //import classNames from './group.module.css';
+import {Card, Col} from 'antd'
+
 
 
 
 export default class Group extends React.PureComponent {
 
-    render(){
+
+    render() {
+
         return (
             <div>
-                This is a Group
+                <Col span={12}>
+                   <Card
+                   title="default size card"
+                   bordered={true}>
+                       <li>
+                           hello
+                       </li>
+                   </Card>
+                </Col>
             </div>
-        )
+
+        );
     }
 }
