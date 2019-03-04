a542541e8ed0ae79b9b37e8ea6f8f20ec5d9c7b5
Nathan <n.malnoury@gmail.com>
2019-03-04 23:00:41 +0100

    Old commit, that did work, I thing

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 
 const config = require('./index');
 
+console.log(config.mongodb.options.user);
 mongoose.connect(config.mongodb.uri, config.mongodb.options);
