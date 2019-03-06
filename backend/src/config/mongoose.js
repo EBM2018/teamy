a54680933e600f755c3711c23d8326139c3eea67
Nathan <n.malnoury@gmail.com>
2019-03-06 11:35:03 +0100

    Forgot about that

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const config = require('./index');
 
-mongoose.connect(config.mongodb.uri, (error) => {
+mongoose.connect(config.mongodb.uri, { useNewUrlParser: true }, (error) => {
   if (error) {
     console.log(error);
   }
