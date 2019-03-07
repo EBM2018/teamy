c16a45bb1721b87786ab1a75a2028bcaf151e544
Nathan <n.malnoury@gmail.com>
2019-03-07 22:16:31 +0100

    Resolve conflict

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const config = require('./index');
 
-<<<<<<< Updated upstream
 mongoose.connect(config.mongodb.uri, { useNewUrlParser: true }, (error) => {
-=======
-mongoose.connect(config.mongodb.uri, config.mongodb.options, (error) => {
->>>>>>> Stashed changes
   if (error) {
     console.log(error);
   }
