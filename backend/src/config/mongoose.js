5d70e1e53c16d8ae4bf7261644995990203fdf70
Nathan <n.malnoury@gmail.com>
2019-03-07 22:15:35 +0100

    Auto stash before merge of "nathandev" and "origin/master"

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const config = require('./index');
 
+<<<<<<< Updated upstream
 mongoose.connect(config.mongodb.uri, { useNewUrlParser: true }, (error) => {
+=======
+mongoose.connect(config.mongodb.uri, config.mongodb.options, (error) => {
+>>>>>>> Stashed changes
   if (error) {
     console.log(error);
   }
