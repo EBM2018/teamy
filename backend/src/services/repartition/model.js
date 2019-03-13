4289f21893479256e80fe5251c022562b8bd6bac
Nathan <n.malnoury@gmail.com>
2019-03-13 11:46:59 +0100

    Add new fields to repartition db
    Which should be called GroupDB

--- backend/src/services/repartition/model.js
+++ backend/src/services/repartition/model.js
@@ -11,6 +11,16 @@ const RepartitionSchema = new mongoose.Schema({
     required: true,
     max: 150,
   },
+  cours: {
+    type: String,
+    required: false,
+    max: 50,
+  },
+  date: {
+    type: Date,
+    required: false,
+  },
+
 });
 
 // Export the model
